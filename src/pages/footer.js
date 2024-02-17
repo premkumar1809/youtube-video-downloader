de74ffeb5340b2667e6cfe0d1f25c315c8396ddf
premkumar1809 <143527746+premkumar1809@users.noreply.github.com>
2024-02-17 17:00:40 +0000

    Update footer.js

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -18,6 +18,7 @@ const Footer = () => {
           </a>
         </p>
         <small>Copyright &copy; YOUTUBE VIDEO DOWNLOADER</small>
+    <a href=""><img alt="banner" src="https://landings-cdn.adsterratech.com/referralBanners/png/80%20x%2030%20px.png" /></a>
       </div>
     </footer>
   );
